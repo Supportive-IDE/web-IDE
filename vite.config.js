b084bce317f5c04fcd6faea029ac938deea92c16
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-01-29 17:45:54 +0000

    updated base for publishing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 
 export default defineConfig({
+    base: "https://supportive-ide.github.io/web-IDE/",
     build: {
         outDir: 'docs',
         emptyOutDir: true, // also necessary
